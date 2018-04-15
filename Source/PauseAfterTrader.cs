045a8203d028e8641f276d7bd5c4c807ff2d5a53
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-15 02:22:17 -0700

    Pause after trader dialogs

---
+++ Source/PauseAfterTrader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(Dialog_Trade), "Close")]
+	public static class PauseAfterTrader
+	{
+		public static void Postfix()
+		{
+			Current.Game.tickManager.CurTimeSpeed = TimeSpeed.Paused;
+		}
+	}
+
+	[HarmonyPatch(typeof(Dialog_Negotiation), "Close")]
+	public static class PauseAfterNegotiation
+	{
+		public static void Postfix(Dialog_Negotiation __instance)
+		{
+			if(__instance is Dialog_Negotiation)	//No override for virtual method
+				Current.Game.tickManager.CurTimeSpeed = TimeSpeed.Paused;
+		}
+	}
+}
