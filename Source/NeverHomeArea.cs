44881c4a066df99552825bb8ba6c686c7190389e
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-15 03:06:43 -0700

    Set an area called "Never Home" and it never gets added to Home

---
+++ Source/NeverHomeArea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(Area_Home), "Set")]
+	public static class NeverHomeArea
+	{
+		//protected virtual void Set(IntVec3 c, bool val)
+		public static bool Prefix(Area_Home __instance, IntVec3 c, bool val)
+		{
+			if (val
+				&& __instance.Map.areaManager.GetLabeled("Never Home") is Area neverHome
+				&& neverHome[c])
+				return false;
+			return true;
+		}
+	}
+}
