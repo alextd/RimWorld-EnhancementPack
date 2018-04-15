6440a272644b9365a61bfd733a2afb2a02184de1
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-15 04:06:45 -0700

    Show selected count for (various)

---
+++ Source/MultiSelectCount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(InspectPaneUtility), "AdjustedLabelFor")]
+	public static class MultiSelectCount
+	{
+		//public static string AdjustedLabelFor(IEnumerable<object> selected, Rect rect)
+		public static void Postfix(ref string __result, IEnumerable<object> selected)
+		{
+			IEnumerable<Thing> things = selected.Where(t => t is Thing).Cast<Thing>();
+			int count = things.Count();
+			if (count == 0) return;
+
+			string label = things.First().LabelCapNoCount;
+			if (things.All(t => t.LabelCapNoCount == label)) return;
+
+			if (count > 1)
+				__result += " x" + count;
+		}
+	}
+}
