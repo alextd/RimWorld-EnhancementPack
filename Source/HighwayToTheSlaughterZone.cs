aa900ec3ee4ecd385faa60c314e652808dd5ceb0
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-10 14:40:24 -0700

    Highway To The Slaughter Zone;
    Make an area named 'Slaughter' and animals are only slaughtered in that area

---
+++ Source/HighwayToTheSlaughterZone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Harmony;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(WorkGiver_Slaughter), "HasJobOnThing")]
+	public static class HighwayToTheSlaughterZone
+	{
+		//public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
+		public static void Postfix(Pawn pawn, Thing t, bool forced, ref bool __result)
+		{
+			if (!__result) return;
+
+			if (pawn.Map.areaManager.GetLabeled("Slaughter") is Area slaughterZone
+					&& !slaughterZone[t.Position])
+				__result = false;
+		}
+	}
+}
