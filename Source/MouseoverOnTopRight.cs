5e87bf1708430ed760f9a64bb4b54dad9c5c5d77
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-10 16:17:16 -0700

    mouseover tile info goes on topright so it's not covered by selection / main tabs
    obviously you should turn this off if you're using the learning helper. TODO: make that an option

---
+++ Source/MouseoverOnTopRight.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+using RimWorld;
+using Harmony;
+using UnityEngine;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(MouseoverReadout), "MouseoverReadoutOnGUI")]
+	public static class MouseoverOnTopRight
+	{
+		//public void MouseoverReadoutOnGUI()
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			List<CodeInstruction> instList = instructions.ToList();
+			for (int i = 0; i < instList.Count; i++)
+			{
+				CodeInstruction inst = instList[i];
+
+				//Topright winter shadow
+				if (inst.opcode == OpCodes.Call && inst.operand == AccessTools.Method(typeof(GenUI), "DrawTextWinterShadow"))
+					inst.operand = AccessTools.Method(typeof(MouseoverOnTopRight), nameof(DrawTextWinterShadowTR));
+
+				//Transform Widgets.Label rect
+				if (inst.opcode == OpCodes.Call && inst.operand == AccessTools.Method(typeof(Widgets), "Label", new Type[] { typeof(Rect), typeof(string) }))
+					inst.operand = AccessTools.Method(typeof(MouseoverOnTopRight), nameof(LabelTransform));
+			
+				yield return inst;
+
+				if (inst.opcode == OpCodes.Callvirt && inst.operand == AccessTools.Property(typeof(MainTabsRoot), "OpenTab").GetGetMethod())
+				{
+					yield return new CodeInstruction(OpCodes.Pop);
+					yield return new CodeInstruction(OpCodes.Ldc_I4_0);// 0 != null is false
+				}
+			}
+		}
+		
+
+		public static void DrawTextWinterShadowTR(Rect badRect)
+		{
+			GenUI.DrawTextWinterShadow(new Rect(UI.screenWidth-256f, 256f, 256f, -256f));
+		}
+
+		public static void LabelTransform(Rect rect, string label)
+		{
+			//rect = new Rect(MouseoverReadout.BotLeft.x, (float)UI.screenHeight - MouseoverReadout.BotLeft.y - num, 999f, 999f);
+			rect.x = UI.screenWidth - rect.x;	//flip x
+			rect.y = UI.screenHeight - rect.y - 50f; //flip y, adjust for maintabs margin: BotLeft.y = 65f, BotLeft.x = 15f
+			rect.x -= Text.CalcSize(label).x;//adjust for text width
+			Widgets.Label(rect, label);
+		}
+	}
+}
