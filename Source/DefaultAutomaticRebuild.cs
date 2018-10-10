bc8b05aa537e5686a1c99e9ff12cd0d83b6a74d9
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-10 14:54:40 -0700

    autorebuild is on by default with new game

---
+++ Source/DefaultAutomaticRebuild.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(PlaySettings), MethodType.Constructor)]
+	public static class DefaultAutomaticRebuild
+	{
+		public static void Postfix(PlaySettings __instance)
+		{
+			__instance.autoRebuild = true;
+		}
+	}
+}
