db3617bc5ca137d679a0b5ca9a56ef94bdee85dc
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-11 01:50:28 -0700

    Flee grenade while drafted

---
+++ Source/FleeGrenadeDrafted.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using Harmony;
+
+namespace TD_Enhancement_Pack
+{
+	public class ThinkNode_ConditionalCanDoDraftedConstantThinkTreeJobNow : ThinkNode_Conditional
+	{
+		protected override bool Satisfied(Pawn pawn)
+		{
+			return !pawn.Downed && !pawn.IsBurning() && !pawn.InMentalState && pawn.Awake();
+		}
+	}
+}
