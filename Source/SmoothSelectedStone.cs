a77ca6072b1f4907252af7df157659f52aa5485d
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-10 20:03:40 -0700

    Selected stone has a button to smooth it (just like the mine button)

---
+++ Source/SmoothSelectedStone.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(ReverseDesignatorDatabase), "InitDesignators")]
+	class SmoothSelectedStone
+	{
+		//private void InitDesignators()
+		public static void Postfix(List<Designator> ___desList)
+		{
+			Designator des = new Designator_SmoothSurface();
+			if (Current.Game.Rules.DesignatorAllowed(des))
+				___desList.Add(des);
+		}
+	}
+
+	[HarmonyPatch(typeof(Designator), "CanDesignateThing")]
+	public static class SmoothCanDesignateThing
+	{
+		//public virtual AcceptanceReport CanDesignateThing(Thing t)
+		public static bool Prefix(ref AcceptanceReport __result, Designator __instance, Thing t)
+		{
+			if (__instance is Designator_SmoothSurface des)
+			{
+				if (EdificeUtility.IsEdifice(t.def) && t.def.IsSmoothable &&
+					__instance.Map.designationManager.DesignationAt(t.Position, DesignationDefOf.SmoothWall) == null)
+					__result = AcceptanceReport.WasAccepted;
+
+				return false;
+			}
+			return true;
+		}
+	}
+
+	[HarmonyPatch(typeof(Designator), "DesignateThing")]
+	public static class SmoothDesignateThing
+	{
+		//public virtual void DesignateThing(Thing t)
+		public static bool Prefix(Designator __instance, Thing t)
+		{
+			if (__instance is Designator_SmoothSurface des)
+			{
+				IntVec3 pos = t.Position;
+				des.Map.designationManager.AddDesignation(new Designation(pos, DesignationDefOf.SmoothWall));
+				des.Map.designationManager.TryRemoveDesignation(pos, DesignationDefOf.Mine);
+
+				return false;
+			}
+			return true;
+		}
+	}
+}
