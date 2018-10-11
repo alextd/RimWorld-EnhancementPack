c074bf17ea45f0e3bb54cdab9720e7ad0b830100
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-10 19:37:32 -0700

    Dropdown designators get their order set by the average of the contained designators

---
+++ Source/DesignatorDropdownOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(Designator_Dropdown), "Add")]
+	public static class DesignatorDropdownOrder
+	{
+		//public void Add(Designator des)
+		public static void Postfix(Designator_Dropdown __instance, List<Designator> ___elements)
+		{
+			__instance.order = ___elements.Sum(d => d.order) / ___elements.Count();
+		}
+	}
+}
