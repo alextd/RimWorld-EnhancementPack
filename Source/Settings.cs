522f741f4dcfb36ddd010dc979709dfd321e06f9
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-16 18:23:32 -0700

    translatable

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -75,7 +75,7 @@ namespace TD_Enhancement_Pack
 			options.CheckboxLabeled("TD.SettingAutoFertility".Translate(), ref autoOverlayFertility);
 			options.CheckboxLabeled("TD.SettingOverlayLighting".Translate(), ref showOverlayLighting, "TD.SettingOverlayLightingDesc".Translate());
 			float beforeO = overlayOpacity;
-			options.SliderLabeled("Lower overlay opacities:", ref overlayOpacity, "{0:P0}");
+			options.SliderLabeled("TD.LowerOverlayOpacities".Translate(), ref overlayOpacity, "{0:P0}");
 			if (beforeO != overlayOpacity)
 				BaseOverlay.SetAllOpacity(overlayOpacity);
 			options.Gap();
