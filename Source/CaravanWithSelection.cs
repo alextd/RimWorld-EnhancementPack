21a56d0008a183021d27304feec96f3018250fe0
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-10-31 03:40:49 -0700

    Add selected things to caravan list when the dialog is opened

---
+++ Source/CaravanWithSelection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(Dialog_FormCaravan), "CalculateAndRecacheTransferables")]
+	static class CaravanWithSelection
+	{
+		//private void CalculateAndRecacheTransferables()
+		public static void Postfix(Dialog_FormCaravan __instance)
+		{
+			foreach (object obj in Find.Selector.SelectedObjectsListForReading.Where(o => o is Thing))
+				if (obj is Thing thing)
+				{
+					Log.Message($"adding {thing}:{thing.stackCount}");
+					TransferableOneWay transferableOneWay = TransferableUtility.TransferableMatching<TransferableOneWay>(thing, __instance.transferables, TransferAsOneMode.PodsOrCaravanPacking);
+					transferableOneWay?.AdjustTo(transferableOneWay.ClampAmount(transferableOneWay.CountToTransfer + thing.stackCount));
+				}
+		}
+	}
+}
