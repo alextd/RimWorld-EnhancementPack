4f42fad40637bc8286f4c79e618905f05c9601cc
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-02-20 19:43:11 -0800

    1.2.1 update news

--- Source/Utilities/HugsLibUpdateNews.cs
+++ Source/Utilities/HugsLibUpdateNews.cs
@@ -12,7 +12,7 @@ namespace TD.Utilities
 {
 	class HugsLibUpdateNews
 	{
-		public static string modVersion = "1.2.0";
+		public static string modVersion = "1.2.1";
 
 
 		public const string UpdateNewsFileDir = "About";
