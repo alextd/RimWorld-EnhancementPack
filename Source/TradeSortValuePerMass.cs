97acd703d1900374a6a56a804145281c93a2c949
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-10 21:49:25 -0700

    TransferableSorterDef for Value/mass

---
+++ Source/TradeSortValuePerMass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace TD_Enhancement_Pack
+{
+	public class TransferableComparer_ValuePerMass : TransferableComparer
+	{
+		public override int Compare(Transferable lhs, Transferable rhs)
+		{
+			return (lhs.AnyThing.MarketValue / lhs.AnyThing.GetStatValue(StatDefOf.Mass))
+				.CompareTo(rhs.AnyThing.MarketValue / rhs.AnyThing.GetStatValue(StatDefOf.Mass));
+		}
+	}
+}
