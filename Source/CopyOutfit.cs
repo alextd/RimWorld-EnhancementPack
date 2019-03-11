8c20791a9d37e97c01c4b2661f13d979f662a3e6
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-10 21:57:37 -0700

    Copy Outfit button

---
+++ Source/CopyOutfit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using Harmony;
+using UnityEngine;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(Dialog_ManageOutfits), nameof(Dialog_ManageOutfits.DoWindowContents))]
+	class CopyOutfit
+	{
+		//public override void DoWindowContents(Rect inRect);
+		public static void Postfix(Rect inRect, ref Outfit ___selOutfitInt)
+		{
+			Rect butRect = new Rect(inRect.width - 150f, 0f, 150f, 35f);//otherwise tediously transpile it in, this dialog isn't using Listing
+			if (Widgets.ButtonText(butRect, "Copy Outfit"))
+			{
+				ThingFilter selFilter = ___selOutfitInt.filter;
+				___selOutfitInt = Current.Game.outfitDatabase.MakeNewOutfit();
+				___selOutfitInt.filter.CopyAllowancesFrom(selFilter);
+			}
+		}
+	}
+}
