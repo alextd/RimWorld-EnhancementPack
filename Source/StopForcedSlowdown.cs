8dbe60dae79ec5ed381a2151099ebdcc2c93d56f
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-04-22 16:34:14 -0700

    Shift-click time controls to stop the forced slowdown

---
+++ Source/StopForcedSlowdown.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Verse;
+using RimWorld;
+using Harmony;
+using UnityEngine;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(TimeControls), nameof(TimeControls.DoTimeControlsGUI))]
+	class StopForcedSlowdown
+	{
+		//public static void DoTimeControlsGUI(Rect timerRect)
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			return Harmony.Transpilers.MethodReplacer(instructions,
+				AccessTools.Method(typeof(Widgets), nameof(Widgets.ButtonImage), new Type[] { typeof(Rect), typeof(Texture2D) }),
+				AccessTools.Method(typeof(StopForcedSlowdown), nameof(ButtonImageAndCheckForShift)));
+		}
+
+		//public static bool ButtonImage(Rect butRect, Texture2D tex)
+		public static FieldInfo forceNormalSpeedUntilInfo = AccessTools.Field(typeof(TimeSlower), "forceNormalSpeedUntil");
+		public static bool ButtonImageAndCheckForShift(Rect butRect, Texture2D tex)
+		{
+			if (Widgets.ButtonImage(butRect, tex))
+			{
+				if(Event.current.shift)
+					forceNormalSpeedUntilInfo.SetValue(Find.TickManager.slower, Find.TickManager.TicksGame - 1);//- 1 to be sure I guess
+				return true;
+			}
+			return false;
+		}
+	}
+}
