fb24d700fbfc39e4d4fcae026d2018e38100e6ad
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-05-14 05:54:52 -0700

    Quickly grab from storage buildings, by

---
+++ Source/QuickGrab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using Harmony;
+
+namespace TD_Enhancement_Pack
+{
+	[HarmonyPatch(typeof(GenPath), "ShouldNotEnterCell")]
+	public static class QuickGrab
+	{
+		//private static bool ShouldNotEnterCell(Pawn pawn, Map map, IntVec3 dest)
+		public static void Postfix(ref bool __result, Pawn pawn, Map map, IntVec3 dest)
+		{
+			if (__result) return;
+
+			if (dest.GetEdifice(map) is Building_Storage)
+				__result = true;
+		}
+	}
+}
