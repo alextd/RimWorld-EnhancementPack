bbb21a949efa62be061bd7e9efb9a33820b8aeec
Allen_Caine <jonas.stark@mail.de>
2021-08-04 23:20:34 +0200

    Adding Adition Harmony Transpiler to modify the changed Notify_DangerousExploderAbouttoExplode Function in RimWorld 1.3 Also added the "launcher" reference to the NOFACTIONNotifyNearbyPawnsOfDangerousExplosive Function to the RimWorld 1.3 changes

--- Source/FleeGrenadeDrafted.cs
+++ Source/FleeGrenadeDrafted.cs
@@ -6,6 +6,9 @@ using Verse;
 using Verse.AI;
 using RimWorld;
 using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
+
 
 namespace TD_Enhancement_Pack
 {
@@ -32,14 +35,57 @@ namespace TD_Enhancement_Pack
 		}
 
 		//public static void NotifyNearbyPawnsOfDangerousExplosive(Thing exploder, DamageDef damage, Faction onlyFaction = null)
-		public static void NOFACTIONNotifyNearbyPawnsOfDangerousExplosive(Thing exploder, DamageDef damage, Faction onlyFaction = null)
+		public static void NOFACTIONNotifyNearbyPawnsOfDangerousExplosive(Thing exploder, DamageDef damage, Faction onlyFaction = null, Thing launcher = null)
 		{
 			Faction actualFaction = onlyFaction;
 			if (Settings.Get().dodgeGrenadeEnemy && onlyFaction != Faction.OfPlayer)
 				actualFaction = null;
 			if (Settings.Get().dodgeGrenadeNPC && onlyFaction == Faction.OfPlayer)
 				actualFaction = null;
-			GenExplosion.NotifyNearbyPawnsOfDangerousExplosive(exploder, damage, actualFaction);
+			GenExplosion.NotifyNearbyPawnsOfDangerousExplosive(exploder, damage, actualFaction, launcher);
 		}
+	}
+
+
+	[HarmonyPatch(typeof(Pawn_MindState), "Notify_DangerousExploderAboutToExplode")]
+	public static class Pawn_MindState_Notify_DangerousExploderAboutToExplode_Patch
+	{
+		/// <summary>
+		/// Harmony Transpiler to change that drafted pawns also move away from explosions
+		/// </summary>
+		/// <explanation>
+		/// The transpiler adds an additional check (CheckDodgeGrenade), which is called from 
+		/// Assembly-CSharp.dll --> Verse.AI.Pawn_MindState.Notify_DangerousExploderAboutToExplode 
+		/// This check is nessecery to the additional condition "&& !this.pawn.Drafted" in Notify_DangerousExploderAbouttoExplode in RimWorld_1.3
+		/// 
+		/// The Transpiler search for the Command "Boolean get_Drafted()" in the IL-Code in Notity_DangerousExploderAboutToExplode
+		/// and adds after this command the call to the CheckDodgeGrenade instead for only checking the draftet status of the pawn.
+		/// </explanation>
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			bool found = false;
+			foreach (var instruction in instructions)
+			{
+				yield return instruction;
+				if ((instruction.operand ?? string.Empty).ToString() == "Boolean get_Drafted()")
+				{
+					yield return new CodeInstruction(OpCodes.Call, m_CheckDodgeGrenade);
+					found = true;
+				}
+			}
+			if (found is false)
+				Verse.Log.Error("Cannot find 'Boolean get_Drafted()' in Pawn_MindState_Notify_DangerousExploderAboutToExplode");
+		}
+		/// <summary>
+		/// If the setting dodgeGrenade is turned on (TRUE) then the function always return FALSE.
+		/// Else the function returns the original drafted value
+		/// </summary>
+		/// <param name="drafted"></param>
+		/// <returns>FALSE for notify pawn about dangerous explosion</returns>
+		public static bool CheckDodgeGrenade(bool drafted)
+		{
+			return !Settings.Get().dodgeGrenade && drafted;
+		}
+		static MethodInfo m_CheckDodgeGrenade = SymbolExtensions.GetMethodInfo(() => CheckDodgeGrenade(true));
 	}
 }
